2bb40dca218e3878565cf3f3152a49d6c0e1897c
ajithkumar.raja@intellectdesign.com <ajithkumar.raja@intellectdesign.com>
2021-03-03 22:52:33 +0530

    Adding console

--- Models/index.js
+++ Models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const MONGO_DB_URL = process.env['MONGO_DB_URL'];
 const DB_NAME = process.env['DB_NAME'];
 
-console.log(MONGO_DB_URL);
+console.log(process.env);
 let database = {
 	connect: async function () {
 		await mongoose.connect(MONGO_DB_URL, {
