c7b77553a9256ed8b6522fac764eec3147cb4198
ajithkumar.raja@intellectdesign.com <ajithkumar.raja@intellectdesign.com>
2021-03-03 22:51:30 +0530

    Changing mongodb url

--- Models/index.js
+++ Models/index.js
@@ -1,7 +1,8 @@
-const mongoose = require("mongoose");
-const MONGO_DB_URL = process.env["MONGO_DB_URL"];
-const DB_NAME = process.env["DB_NAME"];
+const mongoose = require('mongoose');
+const MONGO_DB_URL = process.env['MONGO_DB_URL'];
+const DB_NAME = process.env['DB_NAME'];
 
+console.log(MONGO_DB_URL);
 let database = {
 	connect: async function () {
 		await mongoose.connect(MONGO_DB_URL, {
@@ -10,7 +11,7 @@ let database = {
 			dbName: DB_NAME,
 			useFindAndModify: false,
 		});
-		console.log("connected successfull");
+		console.log('connected successfull');
 	},
 };
 
